6a4bab90bb1e5db0e1f85371bfbefdc5eddaf1c9
OhJuneSang <mktb5576@naver.com>
2021-07-08 17:57:00 +0900

    07/08 2ban 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def hello_world(request):
 	if request.method == "POST":
-		return render(request, 'accountapp/Hello_world.html', context={'text': 'POST METHOD!'})
+		return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
 	else:
-		return render(request, 'accountapp/Hello_world.html', context={'text': 'GET METHOD!'})
-	return render(request, 'accountapp/hello_world.html')
+		return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
