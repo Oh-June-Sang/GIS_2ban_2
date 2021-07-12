919e53042160493ef171379c166988f29c162181
OhJuneSang <mktb5576@naver.com>
2021-07-12 17:12:49 +0900

    07/12 2ban 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,16 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-	if request.method == "POST":
-		temp = request.POST.get('input')
-		new_data =HelloWorld()
-		new_data.text = temp
-		new_data.save()
-		return render(request, 'accountapp/hello_world.html', context={'new_data': new_data})
-	else:
-		return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
+    if request.method == "POST":
+
+        temp = request.POST.get('input')
+        new_data = HelloWorld()
+        new_data.text = temp
+        new_data.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+
+    else:
+        data_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/Hello_world.html', context={'data_list': data_list})
