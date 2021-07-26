f7107c0d3eca86cad7b5ccc666736ce54c43a56e
OhJuneSang <mktb5576@naver.com>
2021-07-26 17:45:55 +0900

    07/26 3

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -5,7 +5,7 @@ from profileapp.views import ProfileCreateView
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view, name='create')
+    path('create/', ProfileCreateView.as_view(), name='create')
 
 
 ]
