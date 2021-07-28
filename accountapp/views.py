882d20060eeca31d335ca970163b0302303ee669
OhJuneSang <mktb5576@naver.com>
2021-07-28 11:58:01 +0900

    07/28 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,11 +35,15 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 has_ownership = [login_required, account_ownership_required]
